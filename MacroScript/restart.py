c91e72d1e3f352119d323007d8aad6e0db325b41
Max Wang <yefeng1@sina.staff.com.cn>
2016-08-08 16:14:52 +0800

    update script

--- MacroScript/restart.py
+++ MacroScript/restart.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('. ../../bin/activate')
+os.system('. ~/flask_proj/bin/activate')
 
 os.system('pkill gunicorn')
 os.system('gunicorn --workers=4 --bind=127.0.0.1:8000 ../index:app')
--- MacroScript/update.py
+++ MacroScript/update.py
@@ -1,3 +1,4 @@
 import os
 
+os.system('cd ~/flask_proj/webapp')
 os.system('git update')
