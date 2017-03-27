1d6cea68119de9f2792f524360efbc6aa165bab9
wangyefeng <wangyefeng@baidu.com>
2017-03-27 13:13:37 +0800

    update

--- source/root.py
+++ source/root.py
@@ -1,4 +1,8 @@
+import gzip
+import functools
+from cStringIO import StringIO as IO
 from werkzeug.routing import BaseConverter
+from flask import after_this_request, request
 
 class RegexConverter(BaseConverter):
     def __init__(self, map, *args):
@@ -12,11 +16,43 @@ class GlobalValue(object):
 app_global = None
 global_value = GlobalValue()
 
+def gzipped(f):
+    @functools.wraps(f)
+    def view_func(*args, **kwargs):
+        @after_this_request
+        def zipper(response):
+            accept_encoding = request.headers.get('Accept-Encoding', '')
+
+            if 'gzip' not in accept_encoding.lower():
+                return response
+
+            response.direct_passthrough = False
+
+            if (response.status_code < 200 or
+                    response.status_code >= 300 or
+                    'Content-Encoding' in response.headers):
+                return response
+            gzip_buffer = IO()
+            gzip_file = gzip.GzipFile(mode='wb', fileobj=gzip_buffer)
+            gzip_file.write(response.data)
+            gzip_file.close()
+
+            response.data = gzip_buffer.getvalue()
+            response.headers['Content-Encoding'] = 'gzip'
+            response.headers['Vary'] = 'Accept-Encoding'
+            response.headers['Content-Length'] = len(response.data)
+
+            return response
+
+        return f(*args, **kwargs)
+
+    return view_func
+
 class AppDelegate():
     __slot__ = {
-        'app'
-        'global_work_path'
-    }
+            'app'
+            'global_work_path'
+            }
     def __init__(self, app=None):
         global app_global
         app_global = app
@@ -39,11 +75,14 @@ class AppDelegate():
         """
 
         @self.app.route('/')
+        @gzipped
         def main():
             return self.app.send_static_file('index.html')
 
         @self.app.route('/<path:path>')
+        @gzipped
         def static_file(path):
+            print(path)
             return self.app.send_static_file(path)
 
 
