4c5708e7fc7d145f4524a7dfcbec67c32adfc1e5
ubuntu <446964321@qq.com>
2017-04-21 18:05:30 +0800

    update

--- MacroScript/update_bundle.sh
+++ MacroScript/update_bundle.sh
@@ -2,10 +2,10 @@
 # . ./update_bundle.sh
 
 if [ ! -d "$HOME/react-blog" ]; then
-    git clone http://github.com/PangPangPangPangPang/react-blog.git
+    git clone http://github.com/PangPangPangPangPang/react-blog.git $HOME
 fi
 
-cp $HOME/react-blog/build/bundle.js $HOME/flask-proj/webapp/static
+cp $HOME/react-blog/build/bundle.js $HOME/flask_proj/webapp/static/bundle.js
 
 . $HOME/flask_proj/webapp/MacroScript/restart.sh
 
