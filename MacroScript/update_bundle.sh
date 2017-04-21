500a44abf2007d97e0ec57c95e28ceb14276e650
ubuntu <446964321@qq.com>
2017-04-21 18:15:57 +0800

    fix bug

--- MacroScript/update_bundle.sh
+++ MacroScript/update_bundle.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/react-blog" ]; then
     git clone http://github.com/PangPangPangPangPang/react-blog.git $HOME
 fi
 
-cd $HONME/react-blog
+cd $HOME/react-blog
 git pull
 
 cp $HOME/react-blog/build/bundle.js $HOME/flask_proj/webapp/static/bundle.js
