00b34278456801544f6cd5f627287fdf0e3a2776
wangyefeng <wangyefeng@baidu.com>
2017-04-21 18:13:51 +0800

    modifu script

--- MacroScript/update_bundle.sh
+++ MacroScript/update_bundle.sh
@@ -5,6 +5,9 @@ if [ ! -d "$HOME/react-blog" ]; then
     git clone http://github.com/PangPangPangPangPang/react-blog.git $HOME
 fi
 
+cd $HONME/react-blog
+git pull
+
 cp $HOME/react-blog/build/bundle.js $HOME/flask_proj/webapp/static/bundle.js
 
 . $HOME/flask_proj/webapp/MacroScript/restart.sh
