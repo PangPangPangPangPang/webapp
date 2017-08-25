a7f7072338892e7ece8d5802409ead50869d8ce3
max <max@Root.com>
2017-08-25 22:45:14 +0800

    update

--- MacroScript/auto_setup.sh
+++ MacroScript/auto_setup.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-# usage:sudo . ./auto_setup.sh -debug
+# usage:. ./auto_setup.sh -debug
 
+
+# prepare
 cd $HOME
 rm -rf flask_proj
 mkdir flask_proj 
@@ -14,6 +16,9 @@ do
         source $HOME/.zshrc
     fi
     if [ "$var" = "-release" ];  then
+        sudo apt-get update
+        sudo apt-get install python-dev python-pip python-virtualenv
+
         sed -i -e "/APP_CONFIG_FILE/d" $HOME/.bashrc
         echo "export APP_CONFIG_FILE='$HOME/flask_proj/webapp/instance/env_release.py'" >> $HOME/.bashrc
         source $HOME/.bashrc
@@ -23,18 +28,42 @@ done
 # download project
 virtualenv flask_proj
 cd flask_proj
-# cp -r $HOME/webapp webapp
 git clone http://github.com/PangPangPangPangPang/webapp.git
+
+# config nginx
+for var in $*
+do
+    if [ "$var" = "-release" ]; then
+        sudo apt-get install nginx
+        cp $HOME/flask_proj/webapp/MacroScript/default /etc/nginx/sites-enabled/
+        sudo service nginx restart
+    fi
+done
+
+# update frontend bundle
+if [ ! -d "$HOME/react-blog" ]; then
+    cd $HOME
+    git clone http://github.com/PangPangPangPangPang/react-blog.git
+else
+    cd $HOME/react-blog
+    git pull
+fi
+
+rm $HOME/flask_proj/webapp/static/*
+cp -r $HOME/react-blog/build/* $HOME/flask_proj/webapp/static/
+cp -r $HOME/react-blog/index.html $HOME/flask_proj/webapp/static/
+cp -r $HOME/flask_proj/webapp/img/* $HOME/flask_proj/webapp/static/
 
 # enter virtualenv
 cd $HOME/flask_proj/bin
 . ./activate
 
-# install env requirements
+# install requirement
 cd $HOME/flask_proj/webapp
 pip install -r requirements.txt
 
 # start gunicorn
 pkill gunicorn
-gunicorn -k flask_sockets.worker -b 127.0.0.1:8000 index:app
+gunicorn --workers=4 --bind=127.0.0.1:8000 index:app
+
 
